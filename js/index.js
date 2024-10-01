8f9c32b3ab5d37489b6ee30ab998b94c47236fa5
joaquin vazquez <joakopapaz@gmail.com>
2024-09-30 21:51:39 -0500

    primera pre entrega :)

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let contraseña;
+
+do{
+    contraseña = prompt("ingresa tu contraseña")
+}while(contraseña !== 'joakopapaz')
+    alert("correcto")
+
+
+
+    let edad = parseInt(prompt("ingresa tu edad nene"))
+
+    if(edad >= 18){
+        console.log("el pive puede entrar")
+    }
+    else{
+        console.log("el pive no entra")
+    }
