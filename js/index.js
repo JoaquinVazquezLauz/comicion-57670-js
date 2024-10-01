a8123ad445dbd9cdde2d9ee471069bbb4d9e3dc2
joaquin vazquez <joakopapaz@gmail.com>
2024-09-30 22:41:04 -0500

    entrega 1

--- js/index.js
+++ js/index.js
@@ -15,3 +15,16 @@ do{
     else{
         console.log("el pive no entra")
     }
+
+
+
+    let nombre1 = prompt("ingresa tu nombre")
+let apellido = prompt("ingresa tu apellido")
+let edad2 = parseFloat(prompt("ingresa tu edad"))
+
+
+function datos(nombre1 = "juan", apellido = "vazquez" , edad2 = 20){
+    console.log("el nombre es " , nombre1 + apellido + edad2)
+}
+
+datos("el nombre es " , nombre1 , apellido + "y su edad es " + edad2)
